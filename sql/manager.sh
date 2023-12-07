5a5d7a1b26fb6fbeba642ea68ce6240904dcfd5d
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-07 16:57:04 -0500

    Update file

--- sql/manager.sh
+++ sql/manager.sh
@@ -58,5 +58,5 @@ sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-data.sql;"
 
 # Run sysbench read and write tests
 sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare
-sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/standalone_benchmarks.txt
+sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/manager_benchmarks.txt
 sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql cleanup
