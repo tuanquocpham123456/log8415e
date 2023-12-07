6d792b2fc37f69c0419d014dbeda4ffe5a10e7c2
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-07 13:50:24 -0500

    Add sudo

--- sql/standalone.sh
+++ sql/standalone.sh
@@ -8,10 +8,10 @@ tar -xvzf sakila-db.tar.gz
 cp -r sakila-db /home/
 
 #Populate database structure
-mysql -u root -e "SOURCE /home/sakila-db/sakila-schema.sql;"
-mysql -u root -e "SOURCE /home/sakila-db/sakila-data.sql;"
+sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-schema.sql;"
+sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-data.sql;"
 
 #Run sysbench read and write tests
-sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare
-sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql --num-threads=6 --max-time=60 --max-requests=0 run >> /home/standalone_benchmarks.txt
-sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql cleanup
+sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare
+sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql --num-threads=6 --max-time=60 --max-requests=0 run >> /home/standalone_benchmarks.txt
+sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql cleanup
