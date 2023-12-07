4ada321f07e7dabbfc36f3e0b866792f7d9d79b6
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-07 16:29:48 -0500

    Update file

--- sql/manager.sh
+++ sql/manager.sh
@@ -1,20 +1,19 @@
-#Install apps
+# Install apps
 sudo apt-get update
 sudo apt-get install -y libncurses5 sysbench
 
-mkdir -p /opt/mysqlcluster/home
+# Set up MySQL Cluster
+sudo mkdir -p /opt/mysqlcluster/home
 cd /opt/mysqlcluster/home
-wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
-tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
+sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
+sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysql
 echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /etc/profile.d/mysqlc.sh
-echo "export PATH=$MYSQLC_HOME/bin:$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
+echo "export PATH=\$MYSQLC_HOME/bin:\$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
 sudo mkdir -p /opt/mysqlcluster/deploy
 cd /opt/mysqlcluster/deploy
-sudo mkdir conf
-sudo mkdir mysqld_data
-sudo mkdir ndb_data
+sudo mkdir conf mysqld_data ndb_data
 cd conf
 
 echo "
@@ -44,21 +43,20 @@ nodeid=4
 [mysqld]
 nodeid=50" | sudo tee -a config.ini
 
-
-sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial -- configdir=/opt/mysqlcluster/deploy/conf/
+sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/
 
 cd /home
 
-#Install sakila
-wget https://downloads.mysql.com/docs/sakila-db.tar.gz
-tar -xvzf sakila-db.tar.gz
+# Install sakila
+sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz
+sudo tar -xvzf sakila-db.tar.gz
 sudo cp -r sakila-db /home/
 
-#Populate database structure
+# Populate database structure
 sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-schema.sql;"
 sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-data.sql;"
 
-#Run sysbench read and write tests
+# Run sysbench read and write tests
 sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare
 sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/standalone_benchmarks.txt
 sudo sysbench oltp_read_write --table-size=100000 --mysql-user=root --mysql-db=sakila --db-driver=mysql cleanup
