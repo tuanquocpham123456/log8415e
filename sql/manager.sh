90a95d598beea8d04954a4be6aa0906bae7f3a6f
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-07 17:44:53 -0500

    Typo

--- sql/manager.sh
+++ sql/manager.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /opt/mysqlcluster/home
 cd /opt/mysqlcluster/home
 sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
-sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysql
+sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /etc/profile.d/mysqlc.sh
 echo "export PATH=\$MYSQLC_HOME/bin:\$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
