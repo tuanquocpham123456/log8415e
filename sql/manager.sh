6503a19c16f3883d5f68fb381191576706b98a4d
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-08 09:41:46 -0500

    Remove && for manager

--- sql/manager.sh
+++ sql/manager.sh
@@ -63,6 +63,6 @@ sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-schema.sql;"
 sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-data.sql;"
 
 # Run sysbench read and write tests
-sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare &&
-sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/manager_benchmarks.txt &&
+sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare
+sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/manager_benchmarks.txt
 sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql cleanup
