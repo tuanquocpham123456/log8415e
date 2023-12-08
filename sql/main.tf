bb607d6a0882a2fa51ae2e419143076fbb3c9c65
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-08 15:43:05 -0500

    Add ingress

--- sql/main.tf
+++ sql/main.tf
@@ -14,6 +14,29 @@ resource "aws_security_group" "final_security_group" {
   name        = "final_security_group"
   description = "My final security group"
 
+  #SSH use
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #HTTP use
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #HTTPS use
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port        = 0
@@ -21,6 +44,21 @@ resource "aws_security_group" "final_security_group" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 1186
+    to_port          = 1186
+    protocol         = "-1"
+    cidr_blocks      = ["172.31.16.0/20"]
   }
 
   egress {
