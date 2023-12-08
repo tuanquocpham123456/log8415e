bbc005d0713de68d23e498a03a8ac77be4197d9a
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-08 09:41:19 -0500

    Update workers code

--- sql/manager.sh
+++ sql/manager.sh
@@ -44,7 +44,6 @@ nodeid=4
 [mysqld]
 nodeid=50" | sudo tee -a config.ini
 
-sudo service mysql restart
 
 cd /opt/mysqlcluster/home/mysqlc
 
@@ -64,6 +63,6 @@ sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-schema.sql;"
 sudo mysql -u root -e "SOURCE /home/sakila-db/sakila-data.sql;"
 
 # Run sysbench read and write tests
-sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare
-sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/manager_benchmarks.txt
+sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql prepare &&
+sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql --threads=6 --time=60 --max-requests=0 run | sudo tee -a /home/manager_benchmarks.txt &&
 sudo sysbench oltp_read_write --table-size=100000 --mysql_storage_engine=ndbcluster --mysql-user=root --mysql-db=sakila --db-driver=mysql cleanup
