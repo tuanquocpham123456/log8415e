bd0dbacb8a121889e7c340d7ccaede4617f32cdd
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-08 16:29:38 -0500

    Add sudo for worker.sh

--- sql/worker.sh
+++ sql/worker.sh
@@ -9,6 +9,6 @@ sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" | sudo tee -a /etc/profile.d/mysqlc.sh
 echo "export PATH=\$MYSQLC_HOME/bin:\$PATH" | sudo tee -a /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
-mkdir -p /opt/mysqlcluster/deploy/ndb_data
+sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
 
 ndbd -c ip-172-31-17-1.ec2.internal:1186
