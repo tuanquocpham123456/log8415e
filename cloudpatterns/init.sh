1ade626e929581686398592a6f9506292fcd7b02
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-18 17:53:11 -0500

    Add dependencies file

---
+++ cloudpatterns/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update the package lists for upgrades and new package installations
+sudo apt-get update
+# Install Python3 and pip3
+sudo apt-get install -y python3 python3-pip
+
+# Install Flask, requests, pymysql, pythonping, and sshtunnel Python libraries
+pip3 install flask requests pymysql pythonping sshtunnel
