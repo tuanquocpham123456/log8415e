ff48a7e932e6ee93a046dc54cb3093db83e98cf5
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-22 08:47:35 -0500

    Update security groups and readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 In order to lunch the infrastructure with all the instances, you need to run ```terraform apply``` inside the **log8415e/sql** folder.
 
 ## MySQL
-Inside all the next instances, you will need to clone the repository before doing the rest of the commands :
+Inside all the next instances, after connecting via ssh, you will need to clone the repository before doing the rest of the commands :
 1. git clone https://github.com/tuanquocpham123456/log8415e.git
 2. cd log8415e/sql
 
@@ -24,4 +24,19 @@ The second command will install Sakila, populate the database structure and run 
 
 This will set up each worker and connect it to the MySQL cluster manager.
 
+## Gatekeeper pattern
+In order to run the gatekeeper pattern, you need to run the following commands inside the gatekeeper instance after connecting via ssh:
+1. git clone https://github.com/tuanquocpham123456/log8415e.git
+2. cd log8415e/cloudpatterns
+3. bash init.sh
+4. bash gatekeeper.sh
+
 ## Proxy pattern
+In order to run the proxy pattern, you need to run the following commands inside the proxy instance after connecting via ssh:
+1. git clone https://github.com/tuanquocpham123456/log8415e.git
+2. cd log8415e/cloudpatterns
+3. bash init.sh
+4. bash proxy.sh
+
+## SQL queries
+Open the Postman app in order to run the SQL queries as a body of a POST or GET request with the gatekeeper public ip adress.
--- sql/main.tf
+++ sql/main.tf
@@ -58,13 +58,13 @@ resource "aws_security_group" "final_security_group" {
     from_port        = 1186
     to_port          = 1186
     protocol         = "tcp"
-    cidr_blocks      = ["172.31.16.0/20"]
+    cidr_blocks      = ["::/0"]
   }
   ingress {
     from_port        = 3306
     to_port          = 3306
     protocol         = "tcp"
-    cidr_blocks      = ["172.31.16.0/20"]
+    cidr_blocks      = ["::/0"]
   }
 
   egress {
@@ -76,8 +76,38 @@ resource "aws_security_group" "final_security_group" {
   }
 }
 
+resource "aws_security_group" "gatekeeper_security_group" {
+  vpc_id      = data.aws_vpc.default.id
+  name        = "gatekeeper_security_group"
+  description = "Gatekeeper security group"
 
-/*resource "aws_instance" "t2_micro_standalone" {
+  #SSH use
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #HTTP use
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #HTTPS use
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "t2_micro_standalone" {
   ami                    = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 20.04 LTS image ID in us-east-1 region
   instance_type          = "t2.micro"
   key_name               = "final_project"
@@ -88,7 +118,7 @@ resource "aws_security_group" "final_security_group" {
   tags                   = {
     Name = "t2_micro_sql_standalone"
   }
-}*/
+}
 
 resource "aws_instance" "t2_micro_manager" {
   ami                    = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 20.04 LTS image ID in us-east-1 region
@@ -133,7 +163,7 @@ resource "aws_instance" "t2_large_gatekeeper" {
   ami                    = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 20.04 LTS image ID in us-east-1 region
   instance_type          = "t2.large"
   key_name               = "final_project"
-  vpc_security_group_ids = [aws_security_group.final_security_group.id]
+  vpc_security_group_ids = [aws_security_group.gatekeeper_security_group.id]
   subnet_id              = "subnet-0cf73552fbe274b6b"
   private_ip             = "172.31.17.21"
   count                  = 1
