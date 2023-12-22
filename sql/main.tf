9e8528acec3ddb882566e013073e5141c374170f
Tuan Quoc Pham <tuanquocpham123@gmail.com>
2023-12-22 17:00:04 -0500

    Remove duplicated code

--- sql/main.tf
+++ sql/main.tf
@@ -47,14 +47,6 @@ resource "aws_security_group" "final_security_group" {
   }
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
     from_port        = 1186
     to_port          = 1186
     protocol         = "tcp"
